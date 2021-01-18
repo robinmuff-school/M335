6e0e2219dbafdc42e9d2ed555d0524cd6805ba47
Robin Muff <robin.muff@bluewin.ch>
2021-01-18 09:30:13 +0100

    Start Powershell update

--- start.ps1
+++ start.ps1
@@ -1,3 +1,13 @@
-ionic build
-npx cap copy
-//npx cap open android
+if ($args[0] -eq "serve") {
+    ionic serve
+}
+if ($args[0] -eq "build") {
+    ionic build
+}
+
+if ($args[1] -eq "true") {
+    npx cap copy
+}
+if ($args[2] -eq "true") {
+    npx cap open android
+}
